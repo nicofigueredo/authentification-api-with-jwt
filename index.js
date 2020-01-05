1205185928ac2b524282fe850df64e230405a851
Nico Figueredo <nico_1507@live.com.ar>
2020-01-05 03:19:42 -0300

    Added db connection

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+
+const authRoute = require("./routes/auth");
 
 dotenv.config();
 
-app.listen(process.env.PORT, () => console.log(`'Server up and running on port ${process.env.PORT}`));
+mongoose.connect(
+    process.env.DB_CONNECTION,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    () => console.log("Connected to db")
+);
+
+//Route Middlewares
+app.use("/api/user", authRoute);
+
+app.listen(process.env.PORT, () =>
+    console.log(`'Server up and running on port ${process.env.PORT}`)
+);
