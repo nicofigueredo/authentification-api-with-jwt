a1a2c9196ac5f3cc15ca8ae9e9ed028b164401ad
Nico Figueredo <nico_1507@live.com.ar>
2020-01-05 16:28:01 -0300

    Added validation to user creation

--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,25 @@
 const router = require("express").Router();
 const User = require("../model/User");
+const { registerValidation } = require("../validation");
 
 router.post("/register", async (req, res) => {
+    //Validation before user creation
+    const { error } = registerValidation(req.body);
+    if (error)
+        return res
+            .status(400)
+            .send({ msg: error.details[0].message.replace(/\"/g, "") });
+
+    //Create a new user
     const user = new User({
         name: req.body.name,
         email: req.body.email,
         password: req.body.password
     });
+
     try {
         const savedUser = await user.save();
-        res.send(savedUser);
+        res.send({ _id: savedUser._id });
     } catch (err) {
         res.status(400).send(err);
     }
